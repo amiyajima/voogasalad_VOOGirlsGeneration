1a15f68a1bde36b467a3120955f8d4dd7a288104
Jesse Ling <jesse.ling@duke.edu>
2014-11-11 21:43:30 -0500

    More Skeleton

---
+++ src/gamedata/goals/PlayerPiecesRemovedGoal.java
@@ -0,0 +1,11 @@
+package gamedata.goals;
+
+public class PlayerPiecesRemovedGoal extends Goal {
+
+	@Override
+	public boolean isWin() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
