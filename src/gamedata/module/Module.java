6168a38ca687e1b4a0d36d77013f6d96b1925884
Jennie Ju <jsj18@duke.edu>
2014-11-12 00:06:38 -0500

    Added Module interface

--- src/gamedata/module/Module.java
+++ src/gamedata/module/Module.java
@@ -1,8 +1,18 @@
 package gamedata.module;
 
-public class Module {
-    public Module () {
+import gamedata.Game;
 
-    }
+/**
+ * Interface of a game component that performs
+ * an action on another component in the game.
+ * Pieces will contain a list of modules.
+ */
+public interface Module {
+	
+	/**
+	 * Executes an action on a component of
+	 * the game (i.e. a piece, patch, or other module)
+	 */
+	public void doBehavior(Game game);
 
 }
