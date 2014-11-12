3ccbc49deafcd79b61818743576caa5a2b5ac163
Rica Zhang <ricagz@yahoo.com>
2014-11-12 00:36:23 -0500

    Movement Module

--- src/gamedata/piece/Piece.java
+++ src/gamedata/piece/Piece.java
@@ -1,7 +1,18 @@
 package gamedata.piece;
 
-public class Piece {
-	public Piece() {
+import gameengine.movement.Movement;
+import java.util.List;
+import javafx.scene.image.ImageView;
 
-	}
+public class Piece {
+    private ImageView myImageView;
+    private List<Movement> myPath;
+    
+    public Piece(ImageView i, List<Movement> m) {
+        myImageView = i;
+    }
+    
+    public ImageView getImageView() {
+        return myImageView;
+    }
 }
