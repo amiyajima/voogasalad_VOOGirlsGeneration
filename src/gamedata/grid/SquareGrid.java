35e89e932369bb7b07816cbca5030f61d25ba826
Jesse Ling <jesse.ling@duke.edu>
2014-11-12 20:42:43 -0500

    SquareGrid

--- src/gamedata/grid/SquareGrid.java
+++ src/gamedata/grid/SquareGrid.java
@@ -1,5 +1,20 @@
 package gamedata.grid;
 
+import gamedata.patch.Patch;
+
 public class SquareGrid extends Grid {
+	private Patch[][] myGrid;
+
+	public SquareGrid(int x, int y) {
+		myGrid = new Patch[x][y];
+	}
+	
+	public Patch getPatch(int x, int y){
+		return myGrid[x][y];
+	}
+	
+	public void setPatch(int x, int y, Patch p){
+		myGrid[x][y] = p;
+	}
 
 }
