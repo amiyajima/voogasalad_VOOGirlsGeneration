0bf8e16ed9b93aa9c4519696ca92b86472d4aee6
Jesse Ling <jesse.ling@duke.edu>
2014-11-14 01:40:38 -0500

    Piece

--- src/gamedata/gamecomponents/Piece.java
+++ src/gamedata/gamecomponents/Piece.java
@@ -5,14 +5,25 @@ import java.util.List;
 import javafx.scene.image.ImageView;
 
 public class Piece {
-    private ImageView myImageView;
-    private List<Movement> myPath;
-    
-    public Piece(ImageView i, List<Movement> m) {
-        myImageView = i;
-    }
-    
-    public ImageView getImageView() {
-        return myImageView;
-    }
+	private int myTypeID;
+	private int myUniqueID;
+	private ImageView myImageView;
+	private List<Movement> myPath;
+
+	public Piece(ImageView i, List<Movement> m) {
+		myImageView = i;
+	}
+
+	public ImageView getImageView() {
+		return myImageView;
+	}
+
+	public int getTypeID() {
+		return myTypeID;
+	}
+
+	public int getUniqueID() {
+		return myUniqueID;
+	}
+
 }
