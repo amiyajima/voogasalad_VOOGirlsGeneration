1620bef345066b196486501a92fa1e335e16e8be
Jesse Ling <jesse.ling@duke.edu>
2014-11-16 16:52:08 -0500

    Tweaking Game Player Level
    1) Game Error Checking
    2) Splitting of Goal Checking and End Turn Checking

--- src/gameengine/player/Player.java
+++ src/gameengine/player/Player.java
@@ -2,27 +2,44 @@ package gameengine.player;
 
 import gamedata.gamecomponents.Level;
 
-
 public class Player {
 
-    private int myNumMovesPlayed;
+	private int myNumMoves;
+	private int myNumMovesPlayed;
 
-    public Player () {
-        myNumMovesPlayed = 0;
-    }
+	public Player() {
+		this(0);
+	}
 
-    /**
-     * While you're playing the level, check after each turn to see if the turn has been won.
-     * If it has been won, tell the game to move to the next level.
-     * 
-     * @param level
-     */
-    public boolean levelWon (Level level) {
-        while (!level.checkTurnEnd(myNumMovesPlayed)) {
-            if (level.levelCompleted()) { return true; }
-            myNumMovesPlayed++;
-        }
-        return false;
-    }
+	public Player(int num) {
+		myNumMovesPlayed = 0;
+		myNumMoves = num;
+	}
+
+	/**
+	 * While you're playing the level, check after each turn to see if the turn
+	 * has been won. If it has been won, tell the game to move to the next
+	 * level.
+	 * 
+	 * @param level
+	 */
+	public boolean levelWon(Level level) {
+		if (level.levelCompleted()) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Checks to see if the player's turn has ended
+	 * 
+	 * @param level
+	 * @return
+	 */
+	public boolean endTurn(Level level) {
+		myNumMovesPlayed++;
+		return level.checkTurnEnd(myNumMovesPlayed);
+
+	}
 
 }
