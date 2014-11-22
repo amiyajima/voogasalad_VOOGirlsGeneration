9bd1d618375fcf548156ac5659e5576584657591
Minkwon Lee <ml241@duke.edu>
2014-11-22 15:23:45 -0500

    implementing a rule

--- src/gamedata/rules/MovableRule.java
+++ src/gamedata/rules/MovableRule.java
@@ -1,5 +1,6 @@
 package gamedata.rules;
 
+import java.util.List;
 import java.util.Map;
 
 
@@ -8,21 +9,31 @@ import java.util.Map;
  */
 public class MovableRule extends Rule {
 
-    private Map<Integer,Integer> myPossiblePatches;
+    private Map<Integer,List<Integer>> myPossiblePatches;
 
     /**
      * @param patches map with keys as state of patch and values as a list of
      * pieces that can be on that patch
      */
-    public MovableRule (Map<Integer,Integer> patches) {
+    public MovableRule (Map<Integer,List<Integer>> patches) {
         myPossiblePatches = patches;
     }
-
+    
+    
     @Override
     public boolean conditionsMet (int patchState) {
     	//TODO: SET THIS TO THE CORRECT LOGIC
 		return false;
         
+    }
+    
+    public boolean conditionsMet (int pieceState, int patchState){
+        for(int i:myPossiblePatches.get(pieceState)){
+            if(i == patchState){
+                return true;
+            }
+        }
+        return false;
     }
 
 }
