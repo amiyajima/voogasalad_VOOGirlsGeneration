d18f78dc7e22f457b8b90a5e22109d69822d28c5
Minkwon Lee <ml241@duke.edu>
2014-11-22 02:07:07 -0500

    modifying MovableRule class

--- src/gamedata/rules/MovableRule.java
+++ src/gamedata/rules/MovableRule.java
@@ -1,26 +1,26 @@
 package gamedata.rules;
 
-import java.util.List;
+import java.util.Map;
 
 
 /**
  * A rule that checks if the piece can move to the given patch 
- *
  */
 public class MovableRule extends Rule {
 
-    private List<Integer> myNoZones;
+    private Map<Integer,Integer> myPossiblePatches;
 
     /**
-     * @param patches list of states of patches that a Piece cannot move on
+     * @param patches map with keys as state of patch and values as a list of
+     * pieces that can be on that patch
      */
-    public MovableRule (List<Integer> patches) {
-        myNoZones = patches;
+    public MovableRule (Map<Integer,Integer> patches) {
+        myPossiblePatches = patches;
     }
 
     @Override
     public boolean conditionsMet (int patchState) {
-        return (myNoZones.contains(patchState));
+        
     }
 
 }
