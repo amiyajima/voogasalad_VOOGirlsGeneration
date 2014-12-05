601845b7a62c4216587bc4706d886bb942607ddf
Jennie Ju <jsj18@duke.edu>
2014-12-05 17:59:50 -0500

    Added shell methods to GUIGrid

--- src/authoring_environment/GUIGrid.java
+++ src/authoring_environment/GUIGrid.java
@@ -3,6 +3,8 @@ package authoring_environment;
 import gamedata.gamecomponents.Patch;
 import gamedata.gamecomponents.Piece;
 
+import java.awt.geom.Point2D;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -12,13 +14,40 @@ import java.util.List;
  *
  */
 public class GUIGrid extends SuperGrid {
-	
+
 	private List<Piece> myPieceData;
 	private List<Patch> myPatchData;
-	
+
 	public GUIGrid(int cols, int rows, int tileSize, String shape) {
 		super(cols,rows,tileSize,shape);
+		myPieceData = new LinkedList<Piece>();
+		myPatchData = new LinkedList<Patch>();
+	}
+
+	protected void addPiece(Piece pieceType, Point2D loc) {
+		// create new instance of pieceType with this location
+		// add to PieceData and set image within tile at this location
+	}
+
+	protected void addPatch(Patch patchType, Point2D loc) {
+		// create new instance of patchType with this location
+		// add to PatchData and set image within tile at this location
+	}
+
+	protected void removePiece(Point2D loc) {
+		// remove piece at this location from data and tiles
 	}
 	
+	protected void removePatch(Point2D loc) {
+		// remove patch at this location from data and tiles
+	}
 	
+	protected void removePieceType(Piece pieceType) {
+		// remove all instances of this type of piece
+	}
+	
+	protected void removePatchType(Patch patchType) {
+		// remove all instances of this type of patch
+	}
+
 }
