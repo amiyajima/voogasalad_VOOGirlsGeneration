e155d937e395b41b2fcb6e2523ffcdf90b020547
Anna Miyajima <am437@duke.edu>
2014-12-04 20:52:46 -0500

    changed player hierarchy

--- src/gameengine/player/Player.java
+++ src/gameengine/player/Player.java
@@ -15,13 +15,10 @@ import java.awt.geom.Point2D;
  * @Author Jesse, Sandy
  */
 
-public class Player {
+public abstract class Player {
 
     private int myNumMovesPlayed;
     private int myID;
-    private Map<KeyCode, Action> myActionKeyMap;
-    // for keypressed, when keys don't trigger actions. ex) arrow keys
-    private Map<KeyCode, Point2D> myMovementKeyMap;
 
     /**
      * Default constructor
@@ -39,12 +36,13 @@ public class Player {
     public Player (int id) {
         myNumMovesPlayed = 0;
         myID = id;
-        setActionKeyMap(myActionKeyMap);
-        setMovementKeyMap(myMovementKeyMap);
+
     }
 
+    public abstract void play ();
+
     /**
-     * Resets number of moves player for the player
+     * Resets number of moves played for the player
      */
     public void resetMovesPlayed () {
         myNumMovesPlayed = 0;
@@ -59,50 +57,12 @@ public class Player {
         return myID;
     }
 
-    /**
-     * used by game player (GUI) so that it knows what action to perform when
-     * certain keycodes are pressed/used.
-     * 
-     * @return myKeyMap which maps actions to pre-defined keycodes
-     */
-    public Map<KeyCode, Action> getActionKeyMap () {
-        return myActionKeyMap;
-    }
-
-    /**
-     * needs to get info from the authoring environment to set up the map..
-     * 
-     * @param myKeyMap
-     */
-    public void setActionKeyMap (Map<KeyCode, Action> myActionKeyMap) {
-        myActionKeyMap = new HashMap<KeyCode, Action>();
-    }
-
-    /**
-     * Returns the Key Mapping for the Player
-     * 
-     * @return
-     */
-    public Map<KeyCode, Point2D> getMovementKeyMap () {
-        return myMovementKeyMap;
-    }
-
-    /**
-     * Sets the Key Mapping for the Player
-     * 
-     * @param myMovementKeyMap2
-     */
-    public void setMovementKeyMap (Map<KeyCode, Point2D> myMovementKeyMap2) {
-        myMovementKeyMap2 = new HashMap<KeyCode, Point2D>();
+    public int getNumMovesPlayed () {
+        return myNumMovesPlayed;
     }
 
     public String toString () {
-        return "ID:" + myID + " NumMovesPlayed:" + myNumMovesPlayed + " movement key map:" +
-               myMovementKeyMap;
-    }
-    
-    public int getNumMovesPlayed(){
-    	return myNumMovesPlayed;
+        return "ID:" + myID + " NumMovesPlayed:" + myNumMovesPlayed;
     }
 
 }
