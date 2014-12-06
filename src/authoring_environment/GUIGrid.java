0e0dca63244d1948b1b3557839fabc2771c20738
Anna Miyajima <am437@duke.edu>
2014-12-06 16:06:22 -0500

    implemented addPiece and addPatch methods in GUIGrid

--- src/authoring_environment/GUIGrid.java
+++ src/authoring_environment/GUIGrid.java
@@ -2,12 +2,12 @@ package authoring_environment;
 
 import gamedata.gamecomponents.Patch;
 import gamedata.gamecomponents.Piece;
-
 import java.awt.geom.Point2D;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
+
 
 /**
  * Authoring, engine, and player may all use this grid!!
@@ -17,107 +17,109 @@ import java.util.Observer;
  */
 public class GUIGrid extends SuperGrid implements Observer {
 
-	private List<Piece> myPieceData;
-	private List<Patch> myPatchData;
+    private List<Piece> myPieceData;
+    private List<Patch> myPatchData;
 
-	public GUIGrid() {
-		super();
-	}
+    public GUIGrid () {
+        super();
+    }
 
-	public GUIGrid(int cols, int rows, int tileSize, String shape) {
-		super(cols, rows, tileSize, shape);
-		myPieceData = new LinkedList<Piece>();
-		myPatchData = new LinkedList<Patch>();
-	}
+    public GUIGrid (int cols, int rows, int tileSize, String shape) {
+        super(cols, rows, tileSize, shape);
+        myPieceData = new LinkedList<Piece>();
+        myPatchData = new LinkedList<Patch>();
+    }
 
-	/**
-	 * Returns number of rows
-	 * 
-	 * @return int number of rows
-	 */
-	public int getNumRows() {
-		return super.myRows;
-	}
+    /**
+     * Returns number of rows
+     * 
+     * @return int number of rows
+     */
+    public int getNumRows () {
+        return super.myRows;
+    }
 
-	/**
-	 * Returns number of columns
-	 * 
-	 * @return int number of columns
-	 */
-	public int getNumCols() {
-		return super.myCols;
-	}
+    /**
+     * Returns number of columns
+     * 
+     * @return int number of columns
+     */
+    public int getNumCols () {
+        return super.myCols;
+    }
 
-	public void addPiece(Piece pieceType, Point2D loc) {
-		// create new instance of pieceType with this location
-		// add to PieceData and set image within tile at this location
-	}
+    public void addPiece (Piece pieceType, Point2D loc) {
+        // create new instance of pieceType with this location
+        // add to PieceData and set image within tile at this location
+        Piece clone = new Piece(pieceType, loc);
+    }
 
-	public void addPatch(Patch patchType, Point2D loc) {
-		// create new instance of patchType with this location
-		// add to PatchData and set image within tile at this location
-	}
+    public void addPatch (Patch patchType, Point2D loc) {
+        // create new instance of patchType with this location
+        // add to PatchData and set image within tile at this location
+        Patch clone = new Patch(patchType, loc);
+    }
 
-	public void removePiece(Piece p) {
-		// remove piece at this location from data and tiles
-	}
+    public void removePiece (Piece p) {
+        // remove piece at this location from data and tiles
+    }
 
-	public void removePatch(Patch p) {
-		// remove patch at this location from data and tiles
-	}
+    public void removePatch (Patch p) {
+        // remove patch at this location from data and tiles
+    }
 
-	public void removePiece(Point2D loc) {
-		// remove piece at this location from data and tiles
-	}
+    public void removePiece (Point2D loc) {
+        // remove piece at this location from data and tiles
+    }
 
-	public void removePatch(Point2D loc) {
-		// remove patch at this location from data and tiles
-	}
+    public void removePatch (Point2D loc) {
+        // remove patch at this location from data and tiles
+    }
 
-	public void removePieceType(Piece pieceType) {
-		// remove all instances of this type of piece
-	}
+    public void removePieceType (Piece pieceType) {
+        // remove all instances of this type of piece
+    }
 
-	public void removePatchType(Patch patchType) {
-		// remove all instances of this type of patch
-	}
+    public void removePatchType (Patch patchType) {
+        // remove all instances of this type of patch
+    }
 
-	public Piece getPiece(Piece p) {
-		return null;
-	}
+    public Piece getPiece (Piece p) {
+        return null;
+    }
 
-	public Patch getPatch(Patch p) {
-		return null;
-	}
-	
-	public Piece getPiece(Point2D loc) {
-		return null;
-	}
+    public Patch getPatch (Patch p) {
+        return null;
+    }
 
-	public Patch getPatch(Point2D loc) {
-		return null;
-	}
+    public Piece getPiece (Point2D loc) {
+        return null;
+    }
 
-	public List<Piece> getPiece(int x, int y) {
-		return null;
-	}
+    public Patch getPatch (Point2D loc) {
+        return null;
+    }
 
-	public List<Patch> getPatch(int x, int y) {
-		return null;
-	}
+    public List<Piece> getPiece (int x, int y) {
+        return null;
+    }
 
-	public List<Piece> getPieces() {
-		return myPieceData;
-	}
+    public List<Patch> getPatch (int x, int y) {
+        return null;
+    }
 
-	public List<Patch> getPatches() {
-		return myPatchData;
-	}
+    public List<Piece> getPieces () {
+        return myPieceData;
+    }
 
-	@Override
-	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
+    public List<Patch> getPatches () {
+        return myPatchData;
+    }
 
-	}
+    @Override
+    public void update (Observable o, Object arg) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
