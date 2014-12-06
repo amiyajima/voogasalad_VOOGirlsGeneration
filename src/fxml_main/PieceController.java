6b9623f849a4547eaefd728b29fc4859afb20bb7
Martin Tamayo <mt153@duke.edu>
2014-12-06 18:22:19 -0500

    Pieces now work with the same functionality as patches.

--- src/fxml_main/PieceController.java
+++ src/fxml_main/PieceController.java
@@ -12,18 +12,26 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import authoring.data.ActionData;
+import authoring.data.PieceData;
 import authoring.data.PieceTypeData;
 import authoring_environment.GUIGrid;
 
 public class PieceController extends GridComponentAbstCtrl<Piece> {
 	
 	private PieceTypeData myPieceTypes;
+	private PieceData myPieces;
 	private ActionData myActionData;
 	
     public PieceController (VBox vbox, ScrollPane propertiesSPane, GUIGrid currGrid, ActionData actions) {
     	super(vbox, propertiesSPane, currGrid);
     	myPieceTypes = new PieceTypeData();
     	myActionData = actions;
+    	
+    	//TODO: myPatchTypes needs to be changed..data..
+    	myPieceTypes = new PieceTypeData();
+    	//TODO: this is just for testing..
+    	currGrid = new GUIGrid(10, 10, 2, "Square Grid");
+    	myPieces = currGrid.getPieces();
     }
 
     @Override
@@ -80,6 +88,17 @@ public class PieceController extends GridComponentAbstCtrl<Piece> {
 				Consumer<Piece> okLambda = (Piece piece) -> {
 					//TODO: Use observables to make all the pieces and
 					// patches in the grid change to fit the updated patch
+					HBox entryBox = myEntryMap.get(entry);
+				    HBox imgNameBox = myIndivEntMap.get(entryBox);
+				    
+				    entryBox.getChildren().remove(imgNameBox);
+				    HBox newImgNameBox = makeEntryBox(piece);	
+				    
+				    entryBox.getChildren().add(newImgNameBox);
+				    myIndivEntMap.replace(entryBox, newImgNameBox);
+				    
+				    myPieceTypes.replace(entry, piece);
+				    myPieces.update(myPieceTypes, entry);
 				};
 				myPropertiesSPane.setContent(new PieceTypeEditor(okLambda, entry, myActionData));
 			}
