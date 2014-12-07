33ec98f36f3aaf1aef1f254a0d8fceac726d70aa
Eric Chen <xc58@duke.edu>
2014-12-07 03:48:36 -0500

    leapmouse implemented

---
+++ src/utilities/FingerTipMouse.java
@@ -0,0 +1,28 @@
+package utilities;
+
+import java.awt.Dimension;
+import java.awt.Robot;
+
+import com.leapmotion.leap.Finger;
+import com.leapmotion.leap.Frame;
+import com.leapmotion.leap.InteractionBox;
+import com.leapmotion.leap.Vector;
+
+import java.awt.Toolkit;
+public class FingerTipMouse implements ILeapMouse{
+
+    @Override
+    public void mouseMove (Frame frame, Robot robot) {
+        InteractionBox box = frame.interactionBox();
+        for(Finger finger: frame.fingers()){
+            if(finger.type() ==Finger.Type.TYPE_INDEX){
+                Vector pos = finger.tipPosition();
+                Vector boxPos = box.normalizePoint(pos);
+                Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+                robot.mouseMove((int) (screen.getWidth()*boxPos.getX()), 
+                        (int) (screen.getHeight()-boxPos.getY()*screen.getHeight()));
+            }
+        }
+    }
+
+}
