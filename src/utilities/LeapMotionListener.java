dac6057df94c224bdc8d668c86c51be412950b07
Eric Chen <xc58@duke.edu>
2014-12-07 04:07:28 -0500

    resourceBundle  added

--- src/utilities/LeapMotionListener.java
+++ src/utilities/LeapMotionListener.java
@@ -1,5 +1,41 @@
 package utilities;
 
-public class LeapMotionListener {
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.util.ResourceBundle;
+
+import com.leapmotion.leap.Controller;
+import com.leapmotion.leap.Frame;
+import com.leapmotion.leap.Gesture;
+import com.leapmotion.leap.Listener;
+
+public class LeapMotionListener extends Listener{
+    ResourceBundle myGestureEffect;
+    Robot myRobot;
+    ILeapMouse myLeapMouse;
+    public static final String GESTURE_FILEPATH = "";
+    
+    public void onConnect (Controller controller){
+    
+        initializeRobot();
+        myGestureEffect = ResourceBundle.getBundle(baseName)
+        enableGestures();
+    }
+    public void enableGestures(){
+        
+    }
+    private void initializeRobot () {
+        try {
+            myRobot = new Robot();
+        } catch (AWTException e) {
+            
+        }
+    }
+    public void onFrame (Controller controller) {
+        Frame frame = controller.frame();
+        myLeapMouse.mouseMove(frame, myRobot);
+        
+    }
+    
 
 }
