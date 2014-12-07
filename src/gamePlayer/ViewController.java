f6090437217a0ead905eb4c82b8cd91cd56c8b67
Rica Zhang <ricagz@yahoo.com>
2014-12-07 13:49:31 -0500

    Changed UI

--- src/gamePlayer/ViewController.java
+++ src/gamePlayer/ViewController.java
@@ -354,11 +354,11 @@ public class ViewController {
 		piece.getActions().forEach(action -> {
 			Label l = new Label(action.toString());
 			l.setOnMouseClicked(event -> bindAction(action));
+			l.getStyleClass().add("label");
 			actions.add(l);
 		});
 
 		controlPane.getChildren().addAll(actions);
-
 	}
 	
 	/**
