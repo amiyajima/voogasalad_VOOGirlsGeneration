bb3972392da462bc00b9934d90b18a78a8c9896a
Minkwon Lee <ml241@duke.edu>
2014-12-07 04:17:19 -0500

    debugging completed. it now works, but as a dummy

--- src/authoring/actionslogic/ActionLogicController.java
+++ src/authoring/actionslogic/ActionLogicController.java
@@ -2,22 +2,15 @@ package authoring.actionslogic;
 
 import gamedata.action.Action;
 import gamedata.action.ConcreteAction;
-import gamedata.events.Event;
-import gamedata.gamecomponents.Piece;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
-import authoring.abstractfeatures.PopupWindow;
-import authoring.concretefeatures.ReceiverEditor;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.VBox;
 
@@ -34,14 +27,14 @@ public class ActionLogicController implements Initializable {
     private VBox myReceiversVBox;
 
     @FXML
-    private Button save;
+    private Button saveButton;
 
     private List<String> myPieceTypes = new ArrayList<String>();
 
     @Override
     // TODO: [IMPORTANT] This constructor will need a List<String> or Set<String> that contains
     // names of Pieces as its argument
-    // also, need a list of actions
+    // also, need a list of existing actions(or names of actions)
     public void initialize (URL location, ResourceBundle resources) {
 
         // for testing
@@ -51,7 +44,6 @@ public class ActionLogicController implements Initializable {
         myPieceTypes.add("Piece A");
         myPieceTypes.add("Piece B");
         myPieceTypes.add("Piece C");
-
         actorsChoiceBox.getItems().addAll(myPieceTypes);
 
         actorsChoiceBox
@@ -62,11 +54,9 @@ public class ActionLogicController implements Initializable {
     }
 
     private void updatePossibleReceivers (String selectedActor) {
-        // TODO Auto-generated method stub
         List<String> myPosReceivers = getReceivers(myPieceTypes, selectedActor);
-//        myReceiversVBox = new VBox();
+        // myReceiversVBox = new VBox();
         for (String p : myPosReceivers) {
-            
             myReceiversVBox.getChildren().add(new CheckBox(p));
         }
     }
