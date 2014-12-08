38d7476f1076f31b7445ddae17ce13261b487115
Minkwon Lee <ml241@duke.edu>
2014-12-08 05:04:18 -0500

    trying to modify/update GamePropertiesData

---
+++ src/authoring/actionslogic/testingGPdata.java
@@ -0,0 +1,32 @@
+package authoring.actionslogic;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import authoring.data.GamePropertiesData;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+
+public class testingGPdata implements Initializable {
+
+    private static final String RESOURCES_PROPERTIES_GRID_SHAPE = "resources.properties/gridShape";
+    GamePropertiesData myGameProperties;
+    @FXML
+    private TextField myNumPlayersField;
+    @FXML
+    private ChoiceBox<String> myGridShapeBox;
+    @FXML
+    private Button mySaveBtn;
+    private int myNumPlayer;
+    private String myGridShape;
+    private ResourceBundle myGridShapeBundle;
+
+    @Override
+    public void initialize (URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        fillGridShapeBox();
+
+    }
