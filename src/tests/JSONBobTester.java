504e4d59cf3bdfe1341642366c9977f1e8423c80
Jesse Ling <jesse.ling@duke.edu>
2014-12-08 08:48:24 -0500

    fixed stuff

--- src/tests/JSONBobTester.java
+++ src/tests/JSONBobTester.java
@@ -88,7 +88,7 @@ public class JSONBobTester {
 		 * Event for garadage comllectin
 		 */
 		Event e1 = new Event("Garbate collectbaon");
-		Condition c = new IsDead("Health");
+		Condition c = new IsDead("health");
 		GlobalAction gl = new DeletePieceAtLocation(new Point2D.Double(0, 0));
 		e1.getConditions().add(c);
 		e1.getGlobalActions().add(gl);
