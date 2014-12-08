9da1e61af81f3e244a1fe24da4a9c6a08de2b391
Mengen Huang <mengen.huang@gmail.com>
2014-12-08 03:44:46 -0500

    experiment with Chinese traslation in gamePlayer

--- src/fxml_main/GUIcontainerController.java
+++ src/fxml_main/GUIcontainerController.java
@@ -1,13 +1,17 @@
 package fxml_main;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import authoring.createedit.GamePropertiesEditor;
 import authoring.data.GamePropertiesData;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.Tab;
 
 public class GUIcontainerController implements Initializable{
 
@@ -16,11 +20,30 @@ public class GUIcontainerController implements Initializable{
 	@FXML
 	private MenuItem gameProperties;
 	
+	@FXML
+	private Tab author1;
+	
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		myGamePropertiesData=new GamePropertiesData();
+//		try {
+//			makeNewTab();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+        
+	}
 
+
+	private void makeNewTab() throws IOException {
+		author1=new Tab();
+		Parent root=null;
+		FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/Voogirls_Authoring.fxml"));
+		root = loader.load();
+        author1.setContent(root);
 	}
 	
 	
