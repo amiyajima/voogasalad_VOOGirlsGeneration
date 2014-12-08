a171511fa78958d5450ea0ec7bf05098191b971b
Jennie Ju <jsj18@duke.edu>
2014-12-08 04:33:37 -0500

    AuthoringController no longer broken, sorry

--- src/authoring/data/GameAuthoringData.java
+++ src/authoring/data/GameAuthoringData.java
@@ -8,40 +8,27 @@ import gamedata.gamecomponents.Game;
  *
  */
 public class GameAuthoringData {
-    private LevelData myLevelData;
-    private PieceTypeData myPieceTypeData;
-    private PatchTypeData myPatchTypeData;
+	private LevelData myLevelData;
+	private PieceTypeData myPieceTypeData;
+	private PatchTypeData myPatchTypeData;
 	private ActionData myActionData;
-    private GamePropertiesData myGamePropertiesData;
-    
-    public GameAuthoringData() {
-    	initData();
-    }
-    
-    private void initData() {
-    	myLevelData = new LevelData();
-        myPieceTypeData = new PieceTypeData();
-        myPatchTypeData = new PatchTypeData();
-    	myActionData = new ActionData();
-        myGamePropertiesData = new GamePropertiesData();
-    }
-    
-    public void initDataPointers(LevelData levelPtr, PieceTypeData pieceTypePtr,
-    		PatchTypeData patchTypePtr, ActionData actionPtr,
-    		GamePropertiesData gamePropertiesPtr) {
-    	levelPtr = myLevelData;
-    	pieceTypePtr = myPieceTypeData;
-    	patchTypePtr = myPatchTypeData;
-    	actionPtr = myActionData;
-    	gamePropertiesPtr = myGamePropertiesData;
-    			
-    }
-    
-    /**
-     * Creates a Game from the stored num players and level data
-     * 
-     * @return Game made from authoring environment data
-     */
+	private GamePropertiesData myGamePropertiesData;
+
+	public GameAuthoringData(LevelData levelData, PieceTypeData pieceTypeData,
+			PatchTypeData patchTypeData, ActionData actionData,
+			GamePropertiesData gamePropertiesData) {
+		myLevelData = levelData;
+		myPieceTypeData = pieceTypeData;
+		myPatchTypeData = patchTypeData;
+		myActionData = actionData;
+		myGamePropertiesData = gamePropertiesData;
+	}
+
+	/**
+	 * Creates a Game from the stored num players and level data
+	 * 
+	 * @return Game made from authoring environment data
+	 */
 	public Game createGame() {
 		int numPlayers = myGamePropertiesData.getNumPlayers();
 		return myLevelData.createGame(numPlayers);
