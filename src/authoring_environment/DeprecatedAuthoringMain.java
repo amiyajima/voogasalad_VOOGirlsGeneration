0cddeb6efa662897ab24acfa3f77dc72fa15ba78
Martin Tamayo <mt153@duke.edu>
2014-12-11 22:32:38 -0500

    Renamed the outdated AuthoringMain to reduce confusion.

--- src/authoring_environment/DeprecatedAuthoringMain.java
+++ src/authoring_environment/DeprecatedAuthoringMain.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class AuthoringMain extends Application{
+public class DeprecatedAuthoringMain extends Application{
 	
 	private static final String STYLESHEET = "/resources/stylesheets/slategray_layout.css";
 	private final String MAIN_TITLE = "Girls GenerEditor";
